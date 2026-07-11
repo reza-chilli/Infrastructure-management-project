67375d5eacd4aee59a64359604a65db93bb40464
moonlight <moonlight@gmail.com>
2026-07-12 01:00:45 +0330

    radios on sidebar for each task

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from src.calculations import run_all_calculations
 from src.data import get_current_year, load_and_preprocess_data
-from src.ui import render_linear_output
+from src.ui import render_linear_output, render_sidebar
 
 st.set_page_config(page_title="Bridge Analysis", layout="wide")
 
@@ -14,4 +14,24 @@ df = load_and_preprocess_data(EXCEL_PATH)
 
 results = run_all_calculations(df, current_year=current_year)
 
-render_linear_output(results)
+page = render_sidebar()
+if page == "Bridge Network Assessment":
+    st.title("Bridge Network Assessment")
+    st.write("This is the Bridge Network Assessment page.")
+
+elif page == "Developement of a Prioritization Framework":
+    st.title("Developement of a Prioritization Framework")
+    st.write("This is the Developement of a Prioritization Framework page.")
+
+elif page == "Maintenance and Rehabilitation Strategy":
+    st.title("Maintenance and Rehabilitation Strategy")
+    st.write("This is the Maintenance and Rehabilitation Strategy page.")
+
+elif page == "Budget Scenario Analysis":
+    st.title("Budget Scenario Analysis")
+    st.write("This is the Budget Scenario Analysis page.")
+
+elif page == "Communication of Results":
+    st.title("Communication of Results")
+    st.write("This is the Communication of Results page.")
+# render_linear_output(results)
--- src/ui.py
+++ src/ui.py
@@ -9,6 +9,25 @@ from src.plots import (
     plot_current_condition_ratings,
 )
 
+def render_sidebar():
+    with st.sidebar:
+        st.title("Bridge Analysis")
+        st.caption("Navigation")
+
+        page = st.radio(
+            "Go to",
+            [
+                "Bridge Network Assessment", 
+                "Developement of a Prioritization Framework", 
+                "Maintenance and Rehabilitation Strategy", 
+                "Budget Scenario Analysis",
+                "Communication of Results"
+            ],
+            index=0
+        )
+
+    return page
+
 
 def render_linear_output(results):
     print('hello from python')
